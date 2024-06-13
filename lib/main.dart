2935137cc9370c14b8c6e3a28fe5ed42a661957b
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2024-06-13 14:08:50 -0600

    trus_location added

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,95 @@
 import 'package:flutter/material.dart';
-import 'package:detect_fake_location/detect_fake_location.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:trust_location/trust_location.dart';
+import 'package:location_permissions/location_permissions.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  String? _latitude;
+  String? _longitude;
+  bool? _isMockLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    requestLocationPermission();
+    TrustLocation.start(5);
+    getLocation();
+  }
+
+  Future<void> getLocation() async {
+    try {
+      TrustLocation.onChange.listen((values) => setState(() {
+            _latitude = values.latitude;
+            _longitude = values.longitude;
+            _isMockLocation = values.isMockLocation;
+          }));
+    } on PlatformException catch (e) {
+      print('PlatformException $e');
+    }
+  }
+
+  void requestLocationPermission() async {
+    PermissionStatus permission =
+        await LocationPermissions().requestPermissions();
+    print('permissions: $permission');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My App'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.black,
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: Colors.white, fontSize: 24.0),
+          bodyMedium: TextStyle(color: Colors.white, fontSize: 20.0),
+        ),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Detect Fake Location'),
-          onPressed: () async {
-            bool isFakeLocation =
-                await DetectFakeLocation().detectFakeLocation();
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text('Fake Location Detected'),
-                  content: Text(
-                      'The user is${isFakeLocation ? '' : ' not'} using a fake location.'),
-                  actions: <Widget>[
-                    TextButton(
-                      child: Text('OK'),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ],
-                );
-              },
-            );
-          },
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Trust Location Plugin'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Mock Location: $_isMockLocation',
+                  style: const TextStyle(
+                      fontSize: 24.0,
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'Latitude: $_latitude, Longitude: $_longitude',
+                  style: const TextStyle(
+                    fontSize: 24.0,
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    letterSpacing: 1.5,
+                    wordSpacing: 2.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
